d09a6063c189c3d2ed3800db52c5017b2d5f59de
OsteroyergoyJA <Oosteroyergoy@gmail.com>
2025-01-10 14:15:14 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# undervisning-10.01.2025
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/OsteroyergoyJA/undervisning-10.01.2025)
---
+++ script.js
@@ -0,0 +1,43 @@
+const header = document.querySelector('#header');
+const button = document.querySelector('#fetch-button');
+
+// async function test() {
+//   console.log('hey');
+//   await new Promise((promise) => setTimeout(promise, 5000));
+//   header.textContent = 'Async';
+// }
+
+async function getData() {
+  let catFacts = [];
+  for (let i = 0; i < 3; i++) {
+    const res = await fetch('https://catfact.ninja/fact');
+    const data = await res.json();
+    catFacts.push(data);
+  }
+  return catFacts;
+}
+
+button.addEventListener('click', async () => {
+  console.log(await getData());
+});
+
+// async function test() {
+//   const facts = await getData();
+//   return facts;
+// }
+// function
+
+// async function showData() {
+//   const facts = await getData();
+//   facts.forEach();
+// }
+
+// test();
+
+// async function getData() {
+//   const response = await fetch('https://catfact.ninja/fact');
+//   const data = await response.json();
+//   header.textContent = data.fact;
+// }
+
+// getData();
